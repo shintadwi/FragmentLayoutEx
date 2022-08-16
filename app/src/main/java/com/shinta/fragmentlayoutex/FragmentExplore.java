27a9a05dfb86b7cf80622ef5a59083c1e65f36a6
shintadwi <shintadwi.suyatno@gmail.com>
2022-08-16 11:29:04 +0800

    Initial commit

---
+++ app/src/main/java/com/shinta/fragmentlayoutex/FragmentExplore.java
@@ -0,0 +1,21 @@
+package com.shinta.fragmentlayoutex;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentExplore extends Fragment {
+    View view;
+    public FragmentExplore() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.explore_fragment,container,false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/shinta/fragmentlayoutex/FragmentHome.java
@@ -0,0 +1,21 @@
+package com.shinta.fragmentlayoutex;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentHome extends Fragment {
+    View view;
+    public FragmentHome() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.home_fragment,container,false);
+        return view;
+    }
+}
